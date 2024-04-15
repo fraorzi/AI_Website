5c1747493ae3cfdecb011b2b7c1501b15556e882
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-04-15 23:43:41 +0200

    adjusted app.jsx for new components and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from './components/Button'
+import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div> 
+        <Header />
+      </div>
+      <ButtonGradient />
+      </>
 }
 
 export default App
