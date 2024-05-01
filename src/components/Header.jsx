a0aade98a3e190dcf9da5573f76d9a3e44829d93
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-01 23:24:20 +0200

    completed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
+import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
 import { brainwave } from "../assets";
+import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
-
 
 const Header = () => {
-
   const pathname = useLocation();
-
   const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
@@ -28,13 +29,17 @@ const Header = () => {
     setOpenNavigation(false);
   };
 
-
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
+
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
@@ -58,11 +63,20 @@ const Header = () => {
               </a>
             ))}
           </div>
+
+          <HamburgerMenu />
         </nav>
-    </div>
-    <Button className="hidden lg:flex" href="#login">
+
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
           Sign in
         </Button>
+
         <Button
           className="ml-auto lg:hidden"
           px="px-3"
@@ -70,8 +84,9 @@ const Header = () => {
         >
           <MenuSvg openNavigation={openNavigation} />
         </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
