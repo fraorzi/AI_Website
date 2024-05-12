24a1d01d6400e3dddd11c42dcdde2796f9109bb2
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-12 23:56:11 +0200

    fixed imports and structure in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import ButtonGradient from "./assets/svg/ButtonGradient"
-import Button from './components/Button'
-import Header from "./components/Header"
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
-function App() {
-
+const App = () => {
   return (
     <>
-      <div> 
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
+        <Footer />
       </div>
-      <ButtonGradient />
-      </>
-}
 
-export default App
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
