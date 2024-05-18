f223c897139be868204075adbea60031356d57fe
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-18 17:16:22 +0200

    added html structure inside section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,6 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 
 const Hero = () => {
-
   const parallaxRef = useRef(null);
 
   return (
@@ -18,8 +17,32 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-     </Section> 
-  )
-}
+      <div className="container relative" ref={parallaxRef}>
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            <span className="inline-block relative">
+              Brainwave{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href="/pricing" white>
+            Get started
+          </Button>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Hero
+export default Hero;
