383bd7b305d252408136600f9ff3820928ab8742
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-25 22:24:26 +0200

    Initial Planning and Basic Structure for Benefits

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Heading from './Heading';
+import Section from './Section';
 
 const Benefits = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Section id="features">
+      <div className="container">
+        <Heading title="Chat Smarter, Not Harder with Brainwave" />
+        <div className="flex flex-wrap gap-10">
+          
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Benefits
+export default Benefits;
