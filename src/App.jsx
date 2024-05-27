a21642fecfeed2adfb565fd50e2b2b04e0367e02
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-27 23:45:42 +0200

    created Collaboration section file and design file for it and added them to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -9,6 +11,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
         <Footer />
       </div>
 
