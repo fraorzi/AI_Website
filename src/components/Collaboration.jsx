022676f06ac2676385d5f76eee74db11a5037caa
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-28 23:48:43 +0200

    Initial commit: Set up basic structure for Collaboration componen

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Section from './Section';
 
 const Collaboration = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+          <ul className="max-w-[22rem] mb-10 md:mb-14">
+            {/* Collaboration content will go here */}
+          </ul>
+          <button>Try it now</button>
+        </div>
+        <div className="lg:ml-auto xl:w-[38rem] mt-4">
+          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+            {/* Collaboration text will go here */}
+          </p>
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  {/* Brainwave symbol will go here */}
+                </div>
+              </div>
+            </div>
+            <ul>
+              {/* Collaboration apps will go here */}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Collaboration
+export default Collaboration;
