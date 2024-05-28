e6345f2ca77b439cb93bc9b0c5beee5e3dc6035a
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-05-28 23:49:55 +0200

    Add final touches and advanced styling to Collaboration component

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { brainwaveSymbol, check } from '../assets';
-import { collabApps, collabContent, collabText } from '../constants';
-import Button from './Button';
-import Section from './Section';
+import { brainwaveSymbol, check } from "../assets";
+import { collabApps, collabContent, collabText } from "../constants";
+import Button from "./Button";
+import Section from "./Section";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
   return (
@@ -73,6 +73,9 @@ const Collaboration = () => {
                 </li>
               ))}
             </ul>
+
+            <LeftCurve />
+            <RightCurve />
           </div>
         </div>
       </div>
