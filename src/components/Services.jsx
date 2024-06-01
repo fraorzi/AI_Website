567b85c4455def1b9311b2d1e6290d7758341dba
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-06-01 23:29:44 +0200

    Set up basic structure for Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from 'react';
+import Section from './Section';
+import Heading from './Heading';
 
 const Services = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI-powered applications"
+        />
+        <div className="relative">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+            </div>
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+              <h4 className="h4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Brainwave unlocks the potential of AI-powered applications
+              </p>
+              <ul className="body-2">
+              </ul>
+            </div>
+          </div>
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Photo editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app&apos;s photo editing feature. Try it now!
+                </p>
+              </div>
+            </div>
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world’s most powerful AI photo and video art generation engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                </ul>
+              </div>
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Services
+export default Services;
