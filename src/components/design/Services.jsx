952e7ea04d092a8b29bef213c69686047aa5e69d
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-06-02 23:39:49 +0200

    started creating design for Services section

---
+++ src/components/design/Services.jsx
@@ -0,0 +1,29 @@
+
+export const Gradient = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export const PhotoChatMessage = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export const VideoChatMessage = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export const VideoBar = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
