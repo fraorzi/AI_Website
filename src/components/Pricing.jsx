94d8a076796e8e39da03906d727dc825176dd222
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-06-10 23:50:35 +0200

    feat: add basic structure for Pricing component

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import Section from "./Section";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-export default Pricing
+export default Pricing;
